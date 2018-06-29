efb467b73d662d5761685c1d31603697f7bda3db
Michael Beach <michaelb@ieee.org>
2018-06-30 01:05:13 +1000

    Filter out all builds for libstdc++.
    Boost is currently not built correctly for the this setting, so don't
    attempt these builds for now.

--- build.py
+++ build.py
@@ -10,5 +10,12 @@ if __name__ == "__main__":
     os.environ["CONAN_BUILD_POLICY"] = "missing"
     
     builder = build_template_default.get_builder(pure_c=False)
-    
+
+    # Revert this change once boost is available for other than libstdc++11.    
+    modified_builds = []
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        if settings["compiler.libcxx"] != "libstdc++":
+            modified_builds.append([settings, options, env_vars, build_requires])
+    builder.builds = modified_builds
+
     builder.run()
